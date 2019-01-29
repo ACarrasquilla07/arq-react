ab17da0e0ad3a7294e4fc58bd458ffb0a6a53226
Raul .A Alzate <raul.alzate@techandsolve.com>
2019-01-29 14:40:23 -0500

    code base

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,7 @@
 
-import React, { useContext, useReducer } from "react";
-import { usePersistedContext, usePersistedReducer } from "../utils/usePersist";
-import StoreTodo from "../context/todo";
-import reducers from "../reducers/todo";
-
-import TodoList from "../components/TodoList";
-import TodoForm from "../components/TodoForm";
+import React from "react";
+import Counter from "../components/Counter";
 
 export default () => {
-    const globalStore = usePersistedContext(useContext(StoreTodo), "state");
-    const [state, dispatch] = usePersistedReducer(useReducer(reducers, globalStore),"state");
-    
-    return (
-      <StoreTodo.Provider value={{ state, dispatch }}>
-        <TodoForm />
-        <TodoList />
-      </StoreTodo.Provider>
-    );
+    return (<div><Counter /></div>);
   }
