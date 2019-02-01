a6468b87f9b0e9d509837d35a3ca0a0a5bec5457
Raul .A Alzate <raul.alzate@techandsolve.com>
2019-02-01 13:43:27 -0500

    config setup

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,25 @@
 
-import React from "react";
-import Counter from "../components/Counter";
+import React, { useState } from "react";
+import { ThemeContext, themes } from "../context/theme";
+import Botton from "../components/Botton";
+
+//TODO: Crear el componente Input con base al componente Botton
+import Input from "../components/Input";
 
 export default () => {
-    return (<div><Counter /></div>);
+    const [myTheme, setMyTheme] = useState(themes.dark);
+    function toggleTheme() {
+        setMyTheme(myTheme === themes.dark
+            ? themes.light
+            : themes.dark);
+    }
+    //TODO: agregar el valor del contexto en el provider del contexto (<ThemeContext.Provider value=...)
+    return (
+        <ThemeContext.Provider >
+            <Botton onClick={toggleTheme}>
+                Change me here!!
+            </Botton> <br />
+            <Input defaultValue={JSON.stringify(myTheme)} size={100}/>
+        </ThemeContext.Provider>
+    );
 }
