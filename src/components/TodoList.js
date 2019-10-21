62b0fada2d355d07edf6e96792592b732bf11b1e
andrescarrasquilla <andres.carrasquilla@techandsolve.com>
2019-10-21 09:17:54 -0500

    Ajustes para eslint

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,19 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
-import { GlobalContext } from "../context/context";
-import TodoHeader from "./TodoHeader";
-import TodoConsumer from "../api/TodoConsumer";
+import React, {
+ useContext, useEffect, useState, useCallback 
+} from 'react';
+import { GlobalContext } from '../context/context';
+import TodoHeader from './TodoHeader';
+import TodoConsumer from '../api/TodoConsumer';
 
 const TodoList = () => {
   const { state, dispatch } = useContext(GlobalContext);
   const [isResult, setIsResult] = useState(null);
 
   function handleComplete(item) {
-    const data = { ...item, complete: true }
+    const data = { ...item, complete: true };
     TodoConsumer.update(data, (status) => {
-      dispatch({ type: "COMPLETE", payload: status.item.id });
-    })
+      dispatch({ type: 'COMPLETE', payload: status.item.id });
+    });
   }
 
 
@@ -19,34 +21,34 @@ const TodoList = () => {
     TodoConsumer.getAll((status) => {
       setIsResult(status.isResult);
       if (status.isResult === 'success') {
-        dispatch({ type: "SET_TODO", payload: status.data });
+        dispatch({ type: 'SET_TODO', payload: status.data });
       }
-    })
+    });
   }, [state.todo]);
 
   return <>{
     {
       success: <List handleComplete={handleComplete} state={state} />,
       error: <div className="alert alert-dark">Error in the request</div>,
-      null: <div>Loading</div>
+      null: <div>Loading</div>,
     }[isResult]
   }</>;
-}
+};
 
 function heander(state) {
-  const pluralize = count =>
-    count > 1 ? `There are ${count} todos.` : `There is ${count} todo.`;
+  const pluralize = (count) => (count > 1 ? `There are ${count} todos.` : `There is ${count} todo.`);
 
   return state.todos.length === 0 ? (
     <h4>Yay! All todos are done! Take a rest!</h4>
   ) : (
-      <TodoHeader>
+    <TodoHeader>
         <span className="float-right">{pluralize(state.todos.length)}</span>
       </TodoHeader>
-    );
+  );
 }
 
-const List = ({ state, handleComplete }) => <div className="row">
+const List = ({ state, handleComplete }) => (
+<div className="row">
   <div className="col-md-12">
     <div className="row">
       <div className="col-md-12">
@@ -73,6 +75,7 @@ const List = ({ state, handleComplete }) => <div className="row">
       </div>
     </div>
   </div>
-</div>;
+</div>
+);
 
 export default TodoList;
